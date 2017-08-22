accc3b34622361e5675838aeee0eb87defd9e1c9
smartlz <1009098051@qq.com>
2017-08-22 17:44:58 +0800

    创建userinfo模型类

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -1,3 +1,11 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    '''
+    用户模型类
+    '''
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=40)
+    email = models.EmailField()
