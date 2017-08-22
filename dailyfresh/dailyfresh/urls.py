e128c9c569d5df048b5b5b1c579203cc89aacc60
smartlz <1009098051@qq.com>
2017-08-22 19:46:20 +0800

    修复bug002

--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+# 修复bug002
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^df_user/', include('df_user.urls')),
